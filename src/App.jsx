19a57f9399fbf3b3a67436911d4746a9b5eac8dd
Yike Du <sy20030260@gmail.com>
2022-02-15 17:31:13 +0800

    feat: add some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [response, setResponse] = useState("");
+  const { register, handleSubmit } = useForm();
+  const login = () => {
+    console.login("login");
+  };
+  const send = (data) => {
+    console.log(data);
+    setResponse(JSON.stringify(data));
+  };
+
   return (
-    <main>
-      React⚛️ + Vite⚡ + Replit🌀
-    </main>
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">🚀 Playground Booster 🚀 </div>
+
+        {currentAccount ? (
+          <div className="text"> {currentAccount} </div>
+        ) : (
+          <button className="button" onClick={login}>
+            Login
+          </button>
+        )}
+
+        <form onSubmit={handleSubmit(send)}>
+          <label> Method </label>
+          <input {...register("method")} />
+          <label> Request </label>
+          <input {...register("request")} />
+          <label> Body </label>
+          <textarea {...register("body")} />
+          <input type="submit" className="button" />
+        </form>
+
+        <label> Response </label>
+        {response ? <div className="text"> {response} </div> : null}
+      </div>
+    </div>
   );
 }
 
